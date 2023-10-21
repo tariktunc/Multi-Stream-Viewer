6f1d57cbd8300d960fb78c959568696de0a5149b
Tarık Tunç <tarktunc@hotmail.com>
2023-10-21 15:53:15 +0300

    settings button

--- src/Components/Settings/settings.js
+++ src/Components/Settings/settings.js
@@ -4,6 +4,7 @@ export default function Settings() {
   const settingHandler = () => {
     console.log("settings");
   };
+
   return (
     <div className="bg-blue-500 absolute bottom-4 right-10 w-24 h-24 rounded-full cursor-pointer">
       <button onClick={settingHandler} type="button" className="h-full w-full">

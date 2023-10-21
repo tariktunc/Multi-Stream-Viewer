217358d4c5472ca3dfcf1ccad97f776da15a0158
Tarık Tunç <tarktunc@hotmail.com>
2023-10-21 15:20:55 +0300

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,12 @@
 "use client";
 import React, { useState } from "react";
-import YoutubeChannels from "@/youtubeChannels/youtubeChannels";
-import Settings from "@/settings/settings";
+import YoutubeChannels from "@/Components/YoutubeChannels/youtubeChannels";
+import Settings from "@/Components/Settings/settings";
 
 export default function Home() {
   return (
     <div>
+      <div>VIEW SETTING </div>
       <Settings />
       <YoutubeChannels />
     </div>

11a2a8d40bdac3b8aa5861c1e66851c9ac98e5fa
Tarık Tunç <tarktunc@hotmail.com>
2023-10-21 15:56:14 +0300

    update

--- src/app/layout.js
+++ src/app/layout.js
@@ -3,7 +3,7 @@ import "./globals.css";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body suppressHydrationWarning={true}>{children}</body>
     </html>
   );
 }

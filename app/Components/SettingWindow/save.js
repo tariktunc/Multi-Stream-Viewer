4128a8fb5d451683bcc7c54d93837a6233336ccd
tariktunc <tarktunc@hotmail.com>
2024-02-23 01:38:52 +0300

    Add clear cookie functionality to Save component

--- app/Components/SettingWindow/save.js
+++ app/Components/SettingWindow/save.js
@@ -1,19 +1,29 @@
 import { Button, Flex } from "@radix-ui/themes";
-import { CheckIcon } from "@radix-ui/react-icons";
+import { CheckIcon, TrashIcon } from "@radix-ui/react-icons";
 
 export default function Save(props) {
-	return (
-		<Flex direction="column" px="9" gap="3">
-			<Button
-				onClick={() => {
-					props.clickSave();
-				}}
-				color="indigo"
-				variant="soft"
-			>
-				<CheckIcon className="cursor-pointer" />
-				SAVE
-			</Button>
-		</Flex>
-	);
+  return (
+    <Flex direction="column" px="9" gap="3">
+      <Button
+        onClick={() => {
+          props.clickSave();
+        }}
+        color="indigo"
+        variant="soft"
+      >
+        <CheckIcon className="cursor-pointer" />
+        SAVE
+      </Button>
+      <Button
+        onClick={() => {
+          props.defaultCookie();
+        }}
+        color="indigo"
+        variant="soft"
+      >
+        <TrashIcon className="cursor-pointer" />
+        Clear Cookie
+      </Button>
+    </Flex>
+  );
 }

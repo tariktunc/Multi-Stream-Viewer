847a8d985a9aef24ba0d7fcd4516ef267ea8f0ff
tariktunc <tarktunc@hotmail.com>
2024-02-23 01:38:58 +0300

    Refactor SettingWindow headers component

--- app/Components/SettingWindow/headers.js
+++ app/Components/SettingWindow/headers.js
@@ -1,17 +1,29 @@
-import React from 'react'
+import React from "react";
 import { Flex, Heading } from "@radix-ui/themes";
-import { Cross1Icon } from "@radix-ui/react-icons";
+import { Cross1Icon, EnterFullScreenIcon } from "@radix-ui/react-icons";
 
 export default function Inputs(props) {
-    return (
-               <Flex >
-                  <Heading as="h3" size="3">
-                     Welcome to MultiChannel
-                  </Heading>
-                  <Cross1Icon
-                     className="cursor-pointer w-10 h-5"
-                     onClick={() => props.setCloseWindow(false)}
-                  />
-               </Flex>
-    )
+  return (
+    <Flex direction="column">
+      <Flex direction="row" justify="end">
+        <EnterFullScreenIcon
+          width={30}
+          height={20}
+          className="cursor-pointer"
+          title="enter full screen"
+          onClick={() => props.fullScreen()}
+        />
+        <Cross1Icon
+          width={30}
+          height={20}
+          className="cursor-pointer"
+          title="close"
+          onClick={() => props.setViewSettingWindow(false)}
+        />
+      </Flex>
+      <Heading as="h3" size="3">
+        Welcome to MultiChannel
+      </Heading>
+    </Flex>
+  );
 }

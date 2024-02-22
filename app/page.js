dbb73327c5db77c96d21d680938cca58878d4688
tariktunc <tarktunc@hotmail.com>
2024-02-23 01:39:09 +0300

    Refactor Home component

--- app/page.js
+++ app/page.js
@@ -4,49 +4,58 @@ import SettingButton from "@/app/Components/SettingButton";
 import SettingWindow from "@/app/Components/SettingWindow";
 
 export default function Home() {
-	const [viewSettingWindow, setViewSettingWindow] = useState(true);
-	const [activeChannel, setActiveChannel] = useState(4);
-	const [data, setData] = useState([]);
+  const [viewSettingWindow, setViewSettingWindow] = useState(false);
+  const [activeChannel, setActiveChannel] = useState(4);
+  const [data, setData] = useState([]);
 
-	const channelGrid = (e) => {
-		switch (e) {
-			case 1:
-				return "grid-cols-1";
-			case 4:
-				return "grid-cols-2";
-			case 9:
-				return "grid-cols-3";
-			case 16:
-				return "grid-cols-4";
-			default:
-				return "grid-cols-1";
-		}
-	};
+  const channelGrid = (e) => {
+    switch (e) {
+      case 1:
+        return "grid-cols-1";
+      case 4:
+        return "grid-cols-2";
+      case 9:
+        return "grid-cols-3";
+      case 16:
+        return "grid-cols-4";
+      default:
+        return "grid-cols-1";
+    }
+  };
 
-
-	return (
-		<main>
-			<div className="msk-container">
-				<div className={`grid ${channelGrid(activeChannel)} justify-content-center align-items-center m-0`}>
-					{data.slice(0, activeChannel).map((channel) => (
-						<div key={channel.id} className="col text-center p-0">
-							<iframe
-								className="d-grid"
-								width="100%"
-								height="100%"
-								src={`https://www.youtube.com/embed/${channel.name}?autoplay=1&mute=1`}
-								title="YouTube video player"
-								allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-							></iframe>
-						</div>
-					))}
-				</div>
-			</div>
-			{viewSettingWindow ? (
-				<SettingWindow activeChannel={activeChannel} setActiveChannel={setActiveChannel} data={data} setData={setData} setViewSettingWindow={setViewSettingWindow} />
-			) : (
-				<SettingButton setViewSettingWindow={setViewSettingWindow} />
-			)}
-		</main>
-	);
+  return (
+    <main>
+      <div className="msk-container">
+        <div
+          className={`grid ${channelGrid(
+            activeChannel
+          )} justify-content-center align-items-center m-0`}
+        >
+          {data.slice(0, activeChannel).map((channel) => (
+            <div key={channel.id} className="col text-center p-0">
+              <iframe
+                className="d-grid"
+                width="100%"
+                height="100%"
+                src={`https://www.youtube.com/embed/${channel.name}?autoplay=1&mute=1`}
+                title="YouTube video player"
+                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+              ></iframe>
+            </div>
+          ))}
+        </div>
+      </div>
+      {viewSettingWindow ? (
+        <SettingWindow
+          activeChannel={activeChannel}
+          setActiveChannel={setActiveChannel}
+          data={data}
+          setData={setData}
+          setViewSettingWindow={setViewSettingWindow}
+        />
+      ) : (
+        <SettingButton setViewSettingWindow={setViewSettingWindow} />
+      )}
+    </main>
+  );
 }
